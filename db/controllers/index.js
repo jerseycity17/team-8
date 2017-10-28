77850bcfaa8548ad9a22b1bebd380a2415c61005
Rupesh Basnet <rupeshbasnet93@gmail.com>
2017-10-28 02:37:07 -0400

    added the MVCfeature list

--- db/controllers/index.js
+++ db/controllers/index.js
@@ -4,5 +4,8 @@ router.use('/login', require('./login'));
 router.use('/signup', require('./signup'));
 router.use('/profile', require('./profile'));
 router.use('/', require('./login'));
+router.use('/home', require('./home'));
+router.use('/home/sendNotification', require('./sendNotification'));
+router.use('/home/checkOnUsers', require('./checkOnUsers'));
 
 module.exports = router;
