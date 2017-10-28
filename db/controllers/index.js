477c99f1958939b0c62a72d66b40915fd0841292
Mikhail Kreytser <Mikhail.kreyster@gmail.com>
2017-10-28 02:03:36 -0400

    layout

--- db/controllers/index.js
+++ db/controllers/index.js
@@ -3,6 +3,6 @@ const router = require('express').Router();
 router.use('/login', require('./login'));
 router.use('/signup', require('./signup'));
 router.use('/profile', require('./profile'));
-router.use('/', require('./home'));
+router.use('/', require('./login'));
 
 module.exports = router;
